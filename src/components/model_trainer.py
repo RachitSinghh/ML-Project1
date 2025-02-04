05c3344ba7217dc3c9a2e9f9a8013f5aec3c20a9
RachitSinghh <singhrachit@outlook.in>
2025-02-04 16:09:31 +0530

    Hyperparameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,6 +48,44 @@ class ModelTrainer:
                 "Adaboost Classifer": AdaBoostRegressor(),
             }    
 
+            # params = {
+            #     "Random Forest":{
+            #         'n_estimators': [8,16,32,64,128,256], 
+            #         "max_depth": [None, 8, 16, 32, 64, 128],
+            #         "min_samples_split": [2, 5, 10],
+            #     },
+            #     "Decision Tree":{
+            #         'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            #         "max_depth": [None, 5,10,20],
+            #         "min_samples_split": [2, 5, 10],
+            #     },
+            #     "Gradient Boosting":{
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,64,128,256],
+            #         'subsample':[0.6,0.7,0.8,0.9],
+            #     },
+            #     "Linear Regression":{},
+            #     "K-Neighbors Classifer":{
+            #         'n_neighbors': [5,7,9,11],
+            #         'weights': ['uniform','distance'],
+            #         'algorithm': ['ball_tree','kd_tree','brute'],
+            #     },
+            #     "XGBClassifer":{
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,64,128,256],
+            #         'max_depth': [3,5,7,10],
+            #     },
+            #     "CatBoosting Classifer":{
+            #         'depth': [6,8,10],
+            #         'learning_rate': [0.01, 0.05, 0.1],
+            #         'n_estimators': [30, 50, 100],
+            #     },
+            #     "Adaboost Classifer":{
+            #         'learning_rate': [.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,62,128,256],
+            #     },
+            # }
+
             model_report: dict=evaluate_models(X_train=X_train, 
                                               y_train=y_train,
                                               X_test=X_test, 
@@ -79,3 +117,4 @@ class ModelTrainer:
         
         except Exception as e:
             raise CustomException(e, sys) # raise an exception if the data is not split
+
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -25,6 +26,29 @@ def save_object(file_path, obj):
 def evaluate_models(X_train,y_train, X_test,y_test,models):
     try: 
         report = {}
+
+        # for model_name, model in models.items():
+        #     # get parameters for current model
+        #     para = params[model_name]
+
+        #     # if parameters exist for the model , perform GridSearchCV
+        #     if para:
+        #         gs = GridSearchCV(model, para, cv=3)
+        #         gs.fit(X_train, y_train)
+
+        #     # use best parameters for the model
+        #     model.set_params(**gs.best_params_)
+
+        #     # Make predictions
+        #     y_train_pred = model.predict(X_train) # Train Model
+        #     y_test_pred = model.predict(X_test)
+
+
+        #     # get R2 scores
+        #     train_model_score = r2_score(y_train, y_train_pred)
+        #     test_model_score = r2_score(y_test, y_test_pred)
+
+        #     report[model_name] = test_model_score
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
